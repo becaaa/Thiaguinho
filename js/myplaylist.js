b879d19fc86fafdb4ac6a6ddfa01e81f220d37bb
becaaa <rebecaesther65@gmail.com>
2017-06-02 14:26:30 -0300

    primeira vez que estou enviando

---
+++ js/myplaylist.js
@@ -0,0 +1,42 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/Thiaguinho_Cancun.mp3',
+        title:'Cancun',
+        artist:'Thiaguinho',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:09',
+        cover:'mix/capa.jpg'
+    },
+
+    {
+        mp3:'mix/Thiaguinho_VamoQueVamo.mp3',
+        title:'Vamo que Vamo',
+        artist:'Thiaguinho',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:01',
+        cover:'mix/capa.jpg'
+    },
+    {
+        mp3:'mix/Thiaguinho_Fotos.mp3',
+        title:'Fotos antigas',
+        artist:'Thiaguinho',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'2:55',
+        cover:'mix/capa.jpg'
+    },
+];
